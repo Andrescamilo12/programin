a6fe0e7e2921f4614c83b3abbea05c3303368a12
Camilo Andres Pulistar Lopez <111613926+Andrescamilo12@users.noreply.github.com>
2022-10-14 17:24:01 -0500

    Add files via upload

---
+++ medida ropa.cs
@@ -0,0 +1,24 @@
+﻿namespace tela
+{
+    public class tela
+    {
+        public static void Main()
+
+        {
+            double pulgada=0.02545, metro;
+            Console.WriteLine("Escriba la cantidad de tela que necesita en metros ");
+            metro = Convert.ToDouble (Console.ReadLine());
+
+            
+
+
+            pulgada = metro / pulgada;
+           
+
+            Console.WriteLine($"la cantidad de tela en pulgadas es:{pulgada}");
+          
+
+
+        }
+    }
+}
